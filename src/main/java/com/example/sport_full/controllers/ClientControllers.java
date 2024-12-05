63e5b29f595a04dc5a4996141dd533759260c6e0
davidgaona <davidgaonahenao@gmail.com>
2024-12-04 20:46:27 -0500

    c3

--- src/main/java/com/example/sport_full/controllers/ClientControllers.java
+++ src/main/java/com/example/sport_full/controllers/ClientControllers.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://magnificent-cajeta-379fff.netlify.app")
 @RestController
 @RequestMapping("/client")
 public class ClientControllers {
